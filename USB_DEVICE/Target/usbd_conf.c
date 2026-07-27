0b87b402c04d65c4a40df5061b39663b11376a37
Josip Mrdeža <ForsakenNydoseth@hotmail.com>
2026-07-27 22:25:42 +0200

    Bi-directional comms/ACK implementation
    Implemented deterministic bi‑directional link initialization using a ping‑pong handshake.
    The first MCU to come online (plane or controller) now sends an initial packet and waits for an acknowledgment from the peer. Once any valid packet is received, it responds with its own ack, establishing a continuous back‑and‑forth exchange.
    The current implementation uses an infinite wait; this will be replaced with a bounded timeout to support long‑distance or delayed links. fix #2

--- USB_DEVICE/Target/usbd_conf.c
+++ USB_DEVICE/Target/usbd_conf.c
@@ -82,7 +82,7 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle)
     */
     GPIO_InitStruct.Pin = GPIO_PIN_11|GPIO_PIN_12;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -91,7 +91,7 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle)
     __HAL_RCC_USB_OTG_FS_CLK_ENABLE();
 
     /* Peripheral interrupt init */
-    HAL_NVIC_SetPriority(OTG_FS_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(OTG_FS_IRQn, 1, 0);
     HAL_NVIC_EnableIRQ(OTG_FS_IRQn);
   /* USER CODE BEGIN USB_OTG_FS_MspInit 1 */
 
